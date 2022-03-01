8af4b450c696554845a789d3f4c9be85dfca4fa9
staniya <staniya@thacher.org>
2022-02-28 20:32:11 -0800

    CT_image_analysis_amplify_settings

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "ctimageanalysis": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
