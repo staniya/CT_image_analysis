605a33efcf94f29bf9ab0dddd7c8217e724d2c22
staniya <staniya@thacher.org>
2022-04-04 20:07:02 -0700

    Adding S3 storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,19 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "function": {
+        "S3Trigger670f7c9d": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "ctimageupload": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
